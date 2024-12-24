07d06d5fa3350246f4c3e19feac7e6679086fd62
Vandanpu Dilip Krishna <154912640+DILIPKRISHNA539@users.noreply.github.com>
2024-12-24 17:06:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def chat():
     return render_template('index.html', user_message=user_message, bot_reply=bot_reply)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=5000)
